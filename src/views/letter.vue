5cb6939bcef4d0a959ab780389c77fd955bd6a40
tcandra24 <tcandra007@gmail.com>
2025-05-23 21:37:29 +0700

    Rev Letter  Width

--- src/views/letter.vue
+++ src/views/letter.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="max-w-xl bg-[#f9f9f9] rounded-lg min-w-2xl text-center align-middle flex items-center p-5 m-5 flex-col gap-5">
+  <div class="max-w-xl bg-[#f9f9f9] rounded-lg text-center align-middle flex items-center p-5 m-5 flex-col gap-5">
     <div class="flex">
       <p class="tangerine-bold text-3xl">Quote</p>
     </div>
-    <p class="tangerine-bold lg:text-[4rem] text-[2rem]">"{{ searchLetter && searchLetter.text }}"</p>
+    <p class="tangerine-bold lg:text-[4rem] text-[3rem]">"{{ searchLetter && searchLetter.text }}"</p>
   </div>
 </template>
 
